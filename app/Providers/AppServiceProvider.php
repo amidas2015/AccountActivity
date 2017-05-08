423a8d8271a8dae93edfe3fa0953117719d3b625
amidas2015 <(amidas2015@gmail.com)>
2017-05-08 10:41:29 +0300

    Fixs SQL error when migrating tables

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        ////
-        Schema::defaultStringLength(191);
+	// Fixs SQL error when migrating tables
+        //Schema::defaultStringLength(191);
     }
 
     /**
