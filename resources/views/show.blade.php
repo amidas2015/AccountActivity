332a390e59dcff6a47589627d5b9f4e640cac531
amidas2015 <(amidas2015@gmail.com)>
2017-05-08 11:18:05 +0300

    changed show.blade

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Last account activity (last 5 transactions):</div>
+                <div class="panel-heading">Account activity (last 5 transactions):</div>
                 <div class="panel-body">
                     <ul class="list-group">
                         @if (count($UserLoginHistory) > 0)
